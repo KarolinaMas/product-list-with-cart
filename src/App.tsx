a62fb37c1d0d05d62237f58c3ccc76c44bb41a45
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-08-06 16:58:16 +0300

    style cart section

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import defaultForEmptyCart from "./assets/illustration-empty-cart.svg";
 
 const App = () => {
   return (
-    <div className="max-w-[327px] mx-auto">
+    <div className="max-w-[327px] mx-auto flex flex-col gap-8">
       <section>
         <h1 className="text-[2.5rem] font-bold">Desserts</h1>
         <ul>
           <li>list item</li>
         </ul>
       </section>
-      <section className="bg-white">
-        <p>Your Cart (0)</p>
-        <img src={defaultForEmptyCart} alt="decorative image for empty cart" />
-        <p>Your added items will appear here</p>
+      <section className="bg-white p-6 rounded-xl">
+        <p className="text-red text-2xl font-bold leading-[125%] mb-6">
+          Your Cart (0)
+        </p>
+        <div className="py-4 flex flex-col items-center gap-4">
+          <img
+            className="w-32 h-32"
+            src={defaultForEmptyCart}
+            alt="decorative image for empty cart"
+          />
+          <p className="text-sm leading-[150%] text-rose-500 font-semibold">
+            Your added items will appear here
+          </p>
+        </div>
       </section>
     </div>
   );
