84c327316e808729e47e7b003c54233b576e8e67
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-08-06 16:09:53 +0300

    set basic structure and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import defaultForEmptyCart from "./assets/illustration-empty-cart.svg";
+
 const App = () => {
-  return <div className="bg-red-400">test</div>;
+  return (
+    <div className="max-w-[327px] mx-auto">
+      <section>
+        <h1 className="text-[2.5rem] font-bold">Desserts</h1>
+        <ul>
+          <li>list item</li>
+        </ul>
+      </section>
+      <section className="bg-white">
+        <p>Your Cart (0)</p>
+        <img src={defaultForEmptyCart} alt="decorative image for empty cart" />
+        <p>Your added items will appear here</p>
+      </section>
+    </div>
+  );
 };
 
 export default App;
